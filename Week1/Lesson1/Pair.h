2870ed591b03faf4f11445dcbbdd10cafbf583a6
MattyB2851 <mtb5te@virginia.edu>
2020-07-06 16:27:12 -0400

    Something is wrong, I can feel it. Definitley the Makefile

---
+++ Week1/Lesson1/Pair.h
@@ -0,0 +1,11 @@
+// the easiest header file in the world. I wish I still had my CS2150 work to show, but this will have to do. 
+#pragma once
+class Pair{
+  public:
+    int a;
+    int b; 
+    int sum();
+};
+
+
+
